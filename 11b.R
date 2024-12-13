603d84c84ed3000671543a1ccc4c8711710bffd4
VANI SATHVIKA REDDY <153749239+vanisathvika@users.noreply.github.com>
2024-12-13 22:14:55 +0530

    Update 11b.R

--- 11b.R
+++ 11b.R
@@ -1,25 +1,14 @@
-#Create a sample of 50 numbers which are incremented
-x<-seq(0,50,by=1)
-#Create a binomial distribution
-y<-dbinom(x,50,0.5)
-#Give a chart file name
-png(file="dbinom.png")
-#Plot the graph for this sample
-plot(x,y)
-#save the file 
+x <- seq(0, 50, by = 1)
+y <- dbinom(x, 50, 0.5)
+png(file = "dbinom.png")
+plot(x, y)
 dev.off()
 
-
-#Probability of gettting 26 or less heads from a 51 tosses of a coin
-x<-pbinom(26,51,0.5)
+x <- pbinom(26, 51, 0.5)
 print(x)
 
+x <- qbinom(0.25, 51, 0.5)
+print(paste("Binomial distribution (quantile):", x))
 
-#How amny heads will have a probality of 0.25 with
-#is tossed 51 times
-x<-qbinom(0.25,51,1/2)
-print(paste("qnomial distribution",x))
-
-#find 8 random values from a sample of 150 with probability of 0.4
-x<-rbinom(8,150,.4)
-print(paste("rbinomal values are:",x))
+x <- rbinom(8, 150, 0.4)
+print(paste("Random binomial values:", x))
