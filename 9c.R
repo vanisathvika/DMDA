dfda70be9f81158ddbfbf7fbfd58231d8eff11d5
VANI SATHVIKA REDDY <153749239+vanisathvika@users.noreply.github.com>
2024-12-14 05:38:17 +0530

    Update 9c.R

--- 9c.R
+++ 9c.R
@@ -1,28 +1,23 @@
-claimants<-read.csv("C:/Users/student/Downloads/claimants.csv")
+claimants <- read.csv("C:/Users/student/Downloads/claimants.csv")
+claimants <- na.omit(claimants)
 
-sum(is.na(claimants))
-
-claimants<-na.omit(claimants)
-
-logit <-glm(ATTORNEY ~ factor(CLMSEX)+factor(CLMINSUR)
-           +factor(SEATBELT)
-           +CLMAGE +LOSS,family = "binomial",
-           data=claimants)
+# Build the logistic regression model
+logit <- glm(ATTORNEY ~ factor(CLMSEX) + factor(CLMINSUR) + factor(SEATBELT) + CLMAGE + LOSS,
+             family = "binomial",
+             data = claimants)
 summary(logit)
-
-prob=predict(logit,type=c("response"),claimants)
-prob
-
-confusion<-table(prob=0.5,claimants$ATTORNEY)
-confusion
-
-Accuracy<-sum(diag(confusion))/sum(confusion)
-Accuracy
+# Predict probabilities
+prob <- predict(logit, type = "response")
+predicted <- ifelse(prob > 0.5, 1, 0)
+confusion <- table(Predicted = predicted, Actual = claimants$ATTORNEY)
+Accuracy <- sum(diag(confusion)) / sum(confusion)
+print(paste("Accuracy:", Accuracy))
 
 library(ROCR)
 library(pROC)
-rocrpred<-prediction(prob,claimants$ATTORNEY)
-rocrpref<-performance(rocrpred,'tpr','fpr')
-plot(rocrpref,colorize=T,text.adj=c(-0.2,1.7))
-auc<-auc(claimants$ATTORNEY~prob)
-auc
+
+rocrpred <- prediction(prob, claimants$ATTORNEY)
+rocrperf <- performance(rocrpred, 'tpr', 'fpr')
+plot(rocrperf, colorize = TRUE, text.adj = c(-0.2, 1.7))
+auc_value <- auc(claimants$ATTORNEY ~ prob)
+print(paste("AUC:", auc_value))
